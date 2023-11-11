71a613cd383da9e28b82d911114aa92303d31333
Maria <mariya.glushanina@gmail.com>
2023-11-11 16:33:56 +0000

    new code with -1 exclusion

--- process.sh
+++ process.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-RATING_AVG=$(awk -F"," 'NR>1 {sum += $NF} END {printf "RATING_AVG %.2f", sum/(NR-1)}' /home/users/datasets/hotels.csv)
+
+RATING_AVG=$(awk -F',' 'NR>1 && $NF != -1 {sum += $NF; count++} END {printf "RATING_AVG %.2f", sum/count}' /home/users/datasets/hotels.csv)
 echo $RATING_AVG
-awk -F',' 'NR>1 {country=tolower($7); hotel_count[country]++} END {for (c in hotel_count) print "HOTELNUMBER " c " " hotel_count[c]}' /home/users/datasets/hotels.csv
-awk -F, '/hilton|hilton/ {split($1, a, "_"); country=tolower(a[1]); hilton_cleanliness[country]+=$11; hilton_count[country]+=1}
-         /holiday_inn/ {split($1, a, "_"); country=tolower(a[1]); holiday_inn_cleanliness[country]+=$11; holiday_inn_count[country]+=1}
+awk -F',' 'NR>1 && $7 != -1 {country=tolower($7); hotel_count[country]++} END {for (c in hotel_count) print "HOTELNUMBER " c " " hotel_count[c]}' /home/users/datasets/hotels.csv
+awk -F, '/hilton|hilton/ && $11 != -1 {split($1, a, "_"); country=tolower(a[1]); hilton_cleanliness[country]+=$11; hilton_count[country]+=1}
+         /holiday_inn/ && $11 != -1 {split($1, a, "_"); country=tolower(a[1]); holiday_inn_cleanliness[country]+=$11; holiday_inn_count[country]+=1}
          END {for (country in hilton_cleanliness) { 
-                  hilton_avg = hilton_cleanliness[country] / hilton_count[country];
-                  holiday_inn_avg = holiday_inn_cleanliness[country] / holiday_inn_count[country];
-                  printf "CLEANLINESS %s %.2f %.2f\n", country, holiday_inn_avg, hilton_avg
+                  if (hilton_count[country] > 0 && holiday_inn_count[country] > 0) {
+                      hilton_avg = hilton_cleanliness[country] / hilton_count[country];
+                      holiday_inn_avg = holiday_inn_cleanliness[country] / holiday_inn_count[country];
+                      printf "CLEANLINESS %s %.2f %.2f\n", country, holiday_inn_avg, hilton_avg
+                  }
               }
          }' /home/users/datasets/hotels.csv
 
