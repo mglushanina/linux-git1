934b8cf2938f43edcf832a92088aba21e0ca63a3
Maria <mariya.glushanina@gmail.com>
2023-11-11 17:52:30 +0000

    let's try again

--- process.sh
+++ process.sh
@@ -4,7 +4,7 @@
 RATING_AVG=$(awk -F',' 'NR>1 && $NF != -1 {sum += $NF; count++} END {printf "RATING_AVG %.2f", sum/count}' /home/users/datasets/hotels.csv)
 echo $RATING_AVG
 awk -F',' 'NR>1 && $7 != -1 {country=tolower($7); hotel_count[country]++} END {for (c in hotel_count) print "HOTELNUMBER " c " " hotel_count[c]}' /home/users/datasets/hotels.csv
-awk -F, '/hilton|hilton/ && $11 != -1 {split($1, a, "_"); country=tolower(a[1]); hilton_cleanliness[country]+=$11; hilton_count[country]+=1}
+awk -F, '/hilton/ && $11 != -1 {split($1, a, "_"); country=tolower(a[1]); hilton_cleanliness[country]+=$11; hilton_count[country]+=1}
          /holiday_inn/ && $11 != -1 {split($1, a, "_"); country=tolower(a[1]); holiday_inn_cleanliness[country]+=$11; holiday_inn_count[country]+=1}
          END {for (country in hilton_cleanliness) { 
                   if (hilton_count[country] > 0 && holiday_inn_count[country] > 0) {
