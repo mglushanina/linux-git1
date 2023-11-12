17588697332b9ab4bbb95f4ea4600ea28b6c899b
Maria <mariya.glushanina@gmail.com>
2023-11-12 16:13:42 +0000

    changes in preprocessing

--- tmp/preprocess.sh
+++ tmp/preprocess.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-# Указываем путь к исходному CSV-файлу и создаем выходной CSV-файл
 input_file="/home/users/datasets/hotels.csv"
-output_file="/tmp/hotels_filtered.csv"
+output_file="/home/mglushanina/linux-git1/tmp/hotels_filtered.csv"
 
-# Используем awk для фильтрации строк и выбора только нужных колонок
-awk -F',' 'BEGIN {OFS=","} NR==1 {print $13, $17} NR>1 && $13 != -1 && $17 != -1 {print $13, $17}' $input_file > $output_file
+# Подсчет строк в исходном файле
+total_lines_before=$(wc -l < "$input_file")
 
-echo "Файл успешно создан: $output_file"
+# Удаление строк с -1 в cleanliness или overall_ratingsource и сохранение только колонок 12 и 18
+awk -F',' 'BEGIN {OFS=","} $12 != -1 && $18 != -1 {gsub(/-1.0/, "-1", $12); gsub(/-1.0/, "-1", $18); print $12, $18}' "$input_file" > "$output_file"
+
+# Подсчет строк в новом файле
+total_lines_after=$(wc -l < "$output_file")
+
+echo "Количество строк в исходном файле: $total_lines_before"
+echo "Количество строк в новом файле: $total_lines_after"
+echo "Файл $output_file создан успешно."
 
